2139e687202a2f3265af5ae289af81338e033dcf
Стас Басов <stas.basov@whatever.com>
2023-12-01 03:41:53 +0500

    changed according to review

--- robots_and_platform.py
+++ robots_and_platform.py
@@ -1,19 +1,17 @@
 """100032587"""
+
 import sys
 
 
-def main() -> int:
+def robots_and_platform(robots_list: list[int],
+                        limit: int) -> int:
     result: int = 0
     actions: int = 0
-    robots_weight_list: list[int] = sorted([int(x) for x in
-                                            sys.stdin.readline().
-                                            rstrip().split(' ')])
-    limit: int = int(sys.stdin.readline().rstrip())
     left_pointer: int = 0
-    right_poiner: int = len(robots_weight_list) - 1
+    right_poiner: int = len(robots_list) - 1
     while left_pointer < right_poiner:
-        pointer_sum: int = (robots_weight_list[left_pointer] +
-                            robots_weight_list[right_poiner])
+        pointer_sum: int = (robots_list[left_pointer] +
+                            robots_list[right_poiner])
         if pointer_sum == limit:
             result += 1
             left_pointer += 1
@@ -28,10 +26,15 @@ def main() -> int:
             left_pointer += 1
             right_poiner -= 1
             actions += 2
-    if actions < len(robots_weight_list):
+    if actions < len(robots_list):
         result += 1
     return result
 
 
 if __name__ == '__main__':
-    main()
+    robots_weight_list: list[int] = [int(robot_weight) for robot_weight in
+                                     sys.stdin.readline().
+                                     rstrip().split(' ')]
+    robots_weight_list.sort()
+    limit: int = int(sys.stdin.readline().rstrip())
+    robots_and_platform(robots_weight_list, limit)
