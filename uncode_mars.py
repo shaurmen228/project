6cd26a3b607ac8fe6d1202ed02899c35408da13b
Стас Басов <stas.basov@whatever.com>
2023-12-12 00:14:11 +0500

    corrected according to review

--- uncode_mars.py
+++ uncode_mars.py
@@ -1,28 +1,24 @@
-"""102538247"""
+"""102608947"""
 import sys
+import string
 
 
-def uncoding_mars(encrypted_string) -> str:
-    stack: list = []
-    temporary_num: int = 0
-    temporary_str: str = ''
+def uncoding_mars(encrypted_string: str) -> str:
+    stack, temporary_num, temporary_line = [], 0, ''
 
     for symbol in encrypted_string:
         if symbol == '[':
-            stack.append(temporary_str)
-            stack.append(temporary_num)
-            temporary_num = 0
-            temporary_str = ''
+            stack.append((temporary_line, temporary_num))
+            temporary_num, temporary_line = 0, ''
         elif symbol == ']':
-            now_factor: int = stack.pop()
-            now_string: str = stack.pop()
-            temporary_str = now_string + now_factor * temporary_str
-        elif symbol.isdigit():
-            temporary_num = temporary_num * 10 + int(symbol)
+            now_line, now_factor = stack.pop()
+            temporary_line = now_line + now_factor * temporary_line
+        elif symbol in string.ascii_letters:
+            temporary_line += symbol
         else:
-            temporary_str += symbol
+            temporary_num = temporary_num * 10 + int(symbol)
 
-    return temporary_str
+    return temporary_line
 
 
 if __name__ == '__main__':
